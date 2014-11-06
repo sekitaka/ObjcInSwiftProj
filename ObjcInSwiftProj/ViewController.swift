4128e7157a1e4b11935ad82de30e289aeec4169b
Takashi Seki <sekitaka.tech@gmail.com>
2014-11-06 20:59:30 +0900

    Initial Commit

---
+++ ObjcInSwiftProj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ObjcInSwiftProj
+//
+//  Created by 崇史 on 2014/11/06.
+//  Copyright (c) 2014年 sekitaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
